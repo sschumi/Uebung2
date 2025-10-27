336d5e41625ae2d9d1762848a02e41cf898d9248
Sarah <sschum29@smail.uni-koeln.de>
2025-10-27 11:02:44 +0100

    Initial commit

---
+++ src/H1_main.java
@@ -0,0 +1,9 @@
+public class H1_main {
+    public static void main(String[] args) {
+        int z = 15;
+        if (z > 10) {
+            z = z-10;
+        }
+        System.out.println(z);
+    }
+}
---
+++ src/H3_main.java
@@ -0,0 +1,29 @@
+public class H3_main {
+    public static void main(String[] args){
+        int i=140, j=150, k=-10;
+        k = -10;
+        if (i > j) {
+            if (i > 200) {
+                if (j > 100) {
+                    k = 3;
+                } else {
+                    k=2;
+                }
+            } else {
+                if (j > 100) {
+                } else {
+                    k=1;
+                }
+            }
+        } else {
+            if (i > 200){
+            } else {
+                if (j > 100){
+                } else {
+                    k=4;
+                }
+            }
+        }
+        System.out.println(k);
+    }
+}
